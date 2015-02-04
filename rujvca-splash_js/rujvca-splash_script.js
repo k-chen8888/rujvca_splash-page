4073b7c4a81cebe7df089dc0bdd8214b1796adb9
Kevin Chen <k.chen8880@gmail.com>
2015-02-03 22:26:20 -0500

    Separated global variables

--- rujvca-splash_js/rujvca-splash_script.js
+++ rujvca-splash_js/rujvca-splash_script.js
@@ -1,42 +1,3 @@
-/* A few global variables so that future eboard members can edit the page freely
- * Follow the format if unsure
- */
-var slides = [];
-
-/* Add a slideshow image as follows
- * 		slides.push("filename.jpg");
- * Check model if unsure
- */
-slides.push("test-imgs/item1.jpg");
-slides.push("test-imgs/item2.jpg");
-slides.push("test-imgs/item3.jpg");
-slides.push("test-imgs/item4.jpg");
-slides.push("test-imgs/item5.jpg");
-
-/* Text that goes in the banner overlay
- * HTML tags allowed, but NO HEADER TAGS (don't use h1, h2, etc.)
- */
-var banner_text = "This is a test";
-
-/* Text for the about the club section
- * HTML tags allowed
- */
-var about_text = "" +
-"<p>" +
-	"No description yet" +
-"</p>" +
-"";
-
-/* Text for the eboard info section
- * HTML tags allowed
- */
-var eboard_text = "" +
-"<p>" +
-	"Election by Russian democracy" +
-"</p>" +
-"";
-
-
 /* Write the document
  * Use given variables above
  */
@@ -75,16 +36,53 @@ var load = function() {
 	 * Sticky when it reaches top
 	 * Images when not active, text when active
 	 */
-	/* First, add div elements to hold the buttons */
-	$("#navbar_container").append("<div id='about-button'></div>");
-	$("#navbar_container").append("<div id='eboard-info-button'></div>");
-	$("#navbar_container").append("<div id='to-blog-button'></div>");
+	/* Add ul to hold button li elements
+	 * Put the container in the correct place
+	 */
+	$("#navbar_container").append("<ul id='navbar'></ul>");
+	$("#navbar_container").css({
+		"position": "absolute",
+		"top": $( document ).height() * .75 + "px"
+	});
+	/* Style this ul to turn it into a navbar */
+	$("#navbar").css({
+		"top": $( document ).height() * .75 + "px",
+		"list-style-type": "none",
+		"margin": "0",
+		"padding": "0",
+	});
+	/* Add an li element for each button */
+	$("#navbar").append("<li id='about-button'></li>");
+	$("#navbar").append("<li id='eboard-info-button'></li>");
+	$("#navbar").append("<li id='to-blog-button'></li>");
+	/* Make the navbar elements more navbar-like */
+	$("#about-button, #eboard-info-button, #to-blog-button").css({
+		"display": "inline-block",
+		"height": $( document ).height() * .1 + "px",
+		"width": $( document ).width() * .15 + "px"
+	});
 	/* Add icons */
 	/* Add text */
-	$("#about-button").append("About the Club");
-	$("#eboard-info-button").append("About the EBoard");
-	$("#to-blog-button").append("<a href='http://rujvca.org'>To the Blog!</a>");
-	/* Add styles */
+	$("#about-button").append("<a id='about-navbutton' href='#'>About the Club</a>");
+	$("#eboard-info-button").append("<a id='eboard-info-navbutton' href='#'>About the E-Board</a>");
+	$("#to-blog-button").append("<a id='to-blog-navbutton' href='http://rujvca.org'>To the Blog!</a>");
+	/* Add universal styles */
+	$("#about-navbutton").position({
+		my: "center",
+		at: "center",
+		of: "#about-button"
+	});
+	$("#eboard-info-navbutton").position({
+		my: "center",
+		at: "center",
+		of: "#eboard-info-button"
+	});
+	$("#to-blog-navbutton").position({
+		my: "center",
+		at: "center",
+		of: "#to-blog-button"
+	});
+	/* Add styles unique to displays of specific sizes */
 	if ( $(window).width() <= 727 ) { /* Mobile */
 		
 	} else if ( $(window).width() > 727 && $(window).width() <= 1010 ) { /* Tablet */
@@ -93,11 +91,35 @@ var load = function() {
 		
 	}
 	
-	/* Add the club description */
+	/* Add the club description and styles */
 	$("#about_club").append(about_text);
+	$("#about_club").css({
+		"position": "absolute",
+		"top": $( document ).height() * .85 + "px",
+		"left": $( document ).width() * .1 + "px",
+		"width": $( document ).width() * .8 + "px"
+	});
+	/* Reveal this element on button click */
+	$("#about-navbutton").click(function() {
+		$("#about_club").fadeTo(500, 1);
+		$("#about_eboard").fadeTo(500, 0);
+	});
 	
-	/* Add the eboard description */
+	/* Add the eboard description and styles */
 	$("#about_eboard").append(eboard_text);
+	$("#about_eboard").css({
+		"position": "absolute",
+		"top": $( document ).height() * .85 + "px",
+		"left": $( document ).width() * .1 + "px",
+		"width": $( document ).width() * .8 + "px"
+	});
+	/* Reveal this element on button click */
+	$("#eboard-info-navbutton").click(function() {
+		$("#about_eboard").fadeTo(500, 1);
+		$("#about_club").fadeTo(500, 0);
+	});
+	/* This starts out hidden */
+	$("#about_eboard").fadeTo(500, 0);
 };
 
 /* Get every image that has loaded and store them in an array
